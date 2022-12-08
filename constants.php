3289ddcbb631cd5317fff34d768b792c4d5bfd5c
Andrew Reynolds <areynold@illinois.edu>
2022-12-08 16:12:54 -0600

    stub freshness filter plugin

---
+++ constants.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace IllinoisPublicMedia\FreshnessFilter;
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed.');
+}
+
+class Constants
+{
+    const NAME = 'Freshness Filter';
+
+    const AUTHOR = 'Illinois Public Media';
+
+    const AUTHOR_URL = 'https://will.illinois.edu';
+
+    const DESCRIPTION = "Check ExpressionEngine channels for freshness.";
+
+    const DOCS_URL = 'https://github.com/willpublicmedia/ee-freshness-filter';
+
+    const MODULE_NAME = 'freshness_filter';
+
+    const NAMESPACE  = 'IllinoisPublicMedia\FreshnessFilter';
+
+    const VERSION = '0.0.0';
+}
---
+++ pi.freshness_filter.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class Freshness_filter
+{
+    public $return_data = '';
+
+    public function __construct()
+    {
+        $this->return_data = $this->filter(
+            ee()->TMPL->fetch_param('channels'),
+            ee()->TMPL->fetch_param('datestring'),
+            ee()->TMPL->fetch_param('newer', true)
+        );
+    }
+
+    public function filter(string $channels, string $datestring, bool $newer)
+    {
+        throw new \Exception('Not implemented');
+    }
+}
