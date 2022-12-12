b01d2b6e80bde24909d33db5c2e66b9ca2869070
Andrew Reynolds <areynold@illinois.edu>
2022-12-12 13:48:16 -0600

    add interval parameter

--- pi.freshness_filter.php
+++ pi.freshness_filter.php
@@ -10,24 +10,27 @@ class Freshness_filter
 
     public function __construct()
     {
-        $channels = ee()->TMPL->fetch_param('channels');
+        $channels = explode('|', ee()->TMPL->fetch_param('channels'));
+        $interval = intval(ee()->TMPL->fetch_param('interval'), '1');
+
         $this->return_data = $this->filter(
-            $channels
+            $channels,
+            $interval
             // ee()->TMPL->fetch_param('datestring'),
             // ee()->TMPL->fetch_param('newer', true)
         );
     }
 
-    public function filter(string $channels/* string $datestring, bool $newer */)
+    public function filter(array $channels, int $interval/*bool $newer */)
     {
         // assume we're filtering newer than 1 year
         $now = new DateTime('now');
         $cutoff = $now->modify('-1 year');
 
         $query = ee()->db->select('channel_name')->from('exp_channels')
-            ->where_in('channel_name', explode('|', $channels))
+            ->where_in('channel_name', $channels)
             ->join('exp_channel_titles', 'exp_channels.channel_id = exp_channel_titles.channel_id', 'Titles')
-            ->where('exp_channel_titles.edit_date >= unix_timestamp(DATE_SUB(now(), interval 1 year))')
+            ->where("exp_channel_titles.edit_date >= unix_timestamp(DATE_SUB(now(), interval $interval year))")
             ->group_by('exp_channel_titles.channel_id')
             ->having('count(exp_channel_titles.channel_id) >= 1');
 
